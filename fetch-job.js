2831fd1ecc1c42a894ca79f749055d8f1a25662e
Daniel Ruswick <danielruswick@Daniels-iMac.local>
2018-06-02 14:50:16 -0500

    added server

--- fetch-job.js
+++ fetch-job.js
@@ -1,52 +1,54 @@
-'use strict';
+const twitter = require('twitter')
+const _ = require('lodash')
+const m_client = require('mongodb').MongoClient;
+// load our local .env file for development
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
 
-let twitter = require('twitter')
-let axios = require('axios')
-let _ = require('lodash')
-const AWS = require('aws-sdk');
 
-AWS.config.update({region: 'us-east-1'});
-
-let ddb = new AWS.DynamoDB.DocumentClient();
-const LIST_ID = '1001854078492774402'
 const twitter_creds = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_TOKEN,
   access_token_secret: process.env.TWITTER_TOKEN_SECRET
 }
+var t_client = new twitter(twitter_creds);
 
-var client = new twitter(twitter_creds);
+
+t_client.get('lists/statuses', {list_id: process.env.TWITTER_LIST_ID, include_entities: true, count: 10}).then((data) => {
+
+  // filter and compress mapped objects from tweet data
+  console.log('about to filter and compress')
+  let tweets =  _.compact(mapToObjects(data)).sort(date_comp)
+
+  console.log('about to write ' + tweets.length + ' tweets to db ' + process.env.MONGO_COLLECTION)
+  if(tweets.length){
+    // Use connect method to connect to the server
+m_client.connect(process.env.MONGO_URL, (e, c) => {
+  console.log("Connected successfully to server");
+  const db = c.db(process.env.MONGO_COLLECTION).collection('tweets')
+  let bulk = db.initializeUnorderedBulkOp()
+  for(let t of tweets){
+    bulk.find({_id: t._id}).upsert().updateOne(t)
+  }
+  bulk.execute().then((res) => {
+    console.log('finished bulk insert', res)
+    c.close()
+  }).catch((e) => {
+    console.log('ERROR EXECUTING BULK', e)
+    c.close
+  })
+  
+})
+  }
+}).catch((e) => {console.log('ERROR: ', e)})
 
 
-exports.handler = async function(event, context, callback){
-  console.log('beginning fetch')
-  console.log('using twitter tokens: ', twitter_creds)
-  await client.get('lists/statuses', {list_id: LIST_ID, include_entities: true, count: 10}).then((data) => {
-    console.log('about to filter and compress')
-    let tweets =  _.compact(mapToObjects(data)).sort(date_comp)
-    console.log('about to write ' + tweets.length + ' tweets to db ' + process.env.TABLE_NAME)
-    if(tweets.length){
-      for (let t of tweets){
-        var params = {
-          'Item' : t,
-          'TableName' : process.env.TABLE_NAME
-        }
-        ddb.put(params, function(e, data) {
-            if(e){
-              console.log('ERROR: ', e)
-            }
-            console.log("data written to dynamodb: " + data);
-          })
-      }
-    }
-  }).then(callback).catch((e) => {console.log('ERROR: ', e)})
-}
-
-let date_comp = (a,b) => {return a.time>b.time ? -1 : a.time<b.time ? 1 : 0}
+let date_comp = (a,b) => {return a.timestamp>b.timestamp ? -1 : a.time<b.time ? 1 : 0}
 
 function mapToObjects(tweets){
   // pull url, timestamp and id if the url exists
-  return tweets.map((t) => {return {'Type': 'Tweet', 'Id': Number(t.id), 'Time': Number(new Date(t.created_at)), 'UserId': Number(t.user.id), 'UserName': t.user.name}})
+  return tweets.map((t) => {return {_id: t.id, timestamp: new Date(t.created_at), user_id: t.user.id, user_name: t.user.name}})
 }
 
--- server.js
+++ server.js
@@ -1,17 +1,41 @@
 const express = require('express')
-const PORT = process.env.PORT || 5000
 const app = express()
+const _ = require('lodash')
+const m_client = require('mongodb').MongoClient;
+// load our local .env file for development
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
 
-app.get('/tweets', (req, res) => {
-    let page = req.query.page ? req.query.page : 1
-    let limit = 10 * page
-    let tweets = []
-    res.send(tweets.reverse().slice((page-1)*10, limit))
-})
+const PORT = process.env.PORT || 5000
+// connect to the mongodb instance
+m_client.connect(process.env.MONGO_URL, (e, c) => {
+    console.log("Connected successfully to mongo server");
+    if(e){
+      console.log('ERROR CONNECTING TO MONGO SERVER', e)
+    }
+    const db = c.db(process.env.MONGO_COLLECTION).collection('tweets')
 
-app.get('/ping', (req, res) => {
-    res.send('pong')
-})
+    // this is the main route for the api
+    app.get('/tweets', (req, res) => {
+        let page = req.query.page ? req.query.page : 1
+        let size = req.query.size ? req.query.size : 10
+        let limit = size * page
+        // fetch tweets and handle pagination
+        let tweets = db.find({timestamp: {$gte: (new Date(new Date() - 1000*60*60*24))}}).sort({timestamp: -1}).limit(page * size).skip((page-1)*size).toArray().then((data) => {
+            console.log(data)
+            res.send(data)
+        }) 
+    })
 
-app.listen(PORT, () => console.log('Example app listening on port 3000!'))
+
+    app.get('/', (req, res) => {res.redirect('/ping')})
+    app.get('/ping', (req, res) => {res.send('pong')})
+
+    app.listen(PORT, () => console.log('server listening'))
+
+
+  })
+
+
 
